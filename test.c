c9581dbdca45c20b287fe80690c59b0a6726f937
Charles Ginane <charles.ginane@epita.fr>
2016-10-19 22:51:31 +0200

    modif Listes_chainées

--- test.c
+++ test.c
@@ -5,7 +5,7 @@
 int main()
 {
   List *list = initialisation();
-  add(list , 42 , 42);
+  //add(list , 42 , 42);
   //printf("%d" , list -> premier -> x );
   //printf("%d" , list -> premier -> y);
   return 0;
