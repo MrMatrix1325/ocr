13da3f603a7297f77cc72d9e5287bc749eaf6e1d
charles Ginane <charles.ginane@epita.fr>
2016-10-30 22:16:50 +0000

    Correction pour la norme

---
+++ image_operation.h
@@ -0,0 +1,19 @@
+# ifndef IMAGE_OPERATION_H_
+# define IMAGE_OPERATION_H_
+
+# include <stdlib.h>
+# include <SDL.h>
+
+Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
+void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
+SDL_Surface* SDL_redim(int W2, int H2, SDL_Surface *img);
+void Bloc_de_caractere(SDL_Surface *img);
+int* Find_min_and_max_pixels(SDL_Surface *img, int i, int j, int *L);
+SDL_Surface* decoupe_image(SDL_Surface *img, int *L);
+SDL_Surface* green(SDL_Surface *img);
+SDL_Surface* green2(SDL_Surface *img);
+int find_next_pixel_notgreen(SDL_Surface *img, int *startx, int *starty);
+int* Find_min_and_max_pixels_ingreenimg(SDL_Surface *img, int x, int y, int *L);
+void Bloc_de_caractere_ingreenimg(SDL_Surface *img, int *startx, int *starty);
+
+# endif
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "image_operation.h"
+
+int main()
+{
+  return 0;
+}
