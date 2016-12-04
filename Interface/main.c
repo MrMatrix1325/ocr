5653350d0287c62abf58be3c8cd0dea07415146a
Charles Ginane <charles.ginane@epita.fr>
2016-12-04 05:35:24 +0100

    Ajout 2 boutons

--- Interface/main.c
+++ Interface/main.c
@@ -1,4 +1,6 @@
-# include "image_operation.h"
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include "pixel_operations.h"
 # include <err.h>
 # include <stdlib.h>
 # include <gtk/gtk.h>
@@ -10,6 +12,9 @@ struct param{
   char * path;
   int grey;
   int black;
+  int green;
+  int green2;
+  int bin;
 };
 
 void error()
@@ -95,6 +100,12 @@ void save_img(GtkWidget *button, struct param *para)
     {
       img = grey_lvl(img);
     }
+    else if(para->green)
+      img = green(img);
+    else if (para->green2)
+      img = green2(img);
+    else if(para->bin)
+			binarize(img);
     SDL_SaveBMP(img,path);
    
   }
@@ -118,6 +129,9 @@ void reset_img(GtkWidget *button, struct param *para)
   gtk_image_set_from_file(GTK_IMAGE(para->img), "$-redimreset");
   para->grey = 0;
   para->black = 0;
+  para->green = 0;
+  para->green2 = 0;
+  para->bin = 0;
   free(img);
   remove("$-redimreset");
 
@@ -137,6 +151,10 @@ void grey (GtkWidget *button , struct param *para)
   SDL_SaveBMP(img, "$-greyimg");
   gtk_image_set_from_file(GTK_IMAGE(para->img), "$-greyimg");
   para->grey = 1;
+  para->black = 0;
+  para->green = 0;
+  para->green2 = 0;
+  para->bin = 0;
   remove("$-greyimg");
   free(img);
 }
@@ -155,11 +173,83 @@ void black (GtkWidget *button, struct param *para)
   img = SDL_redim(1100,600,img);
   SDL_SaveBMP(img, "$-blackimg");
   para->black = 1;
+  para->green = 0;
+  para->grey = 0;
+  para->green2 = 0;
+  para->bin = 0;
   gtk_image_set_from_file(GTK_IMAGE(para->img), "$-blackimg");
   remove("$-blackimg");
   free(img);
 
 } 
+
+
+void to_green (GtkWidget *button , struct param *para)
+{
+  if (para->path == NULL)
+  {
+    return;
+  }
+  SDL_Surface *img;
+  para->but = button;
+  img = load_image(para->path);
+  img = green(img);
+  img = SDL_redim(1100,600,img);
+  SDL_SaveBMP(img, "$-greenimg");
+  gtk_image_set_from_file(GTK_IMAGE(para->img), "$-greenimg");
+  para->green = 1;
+  para->green2 = 0;
+  para->black = 0;
+  para->grey = 0;
+  para->bin = 0;
+  remove("$-greenimg");
+  free(img);
+}
+
+void to_green2 (GtkWidget *button , struct param *para)
+{
+  if (para->path == NULL)
+  {
+    return;
+  }
+  SDL_Surface *img;
+  para->but = button;
+  img = load_image(para->path);
+  img = green2(img);
+  img = SDL_redim(1100,600,img);
+  SDL_SaveBMP(img, "$-green2img");
+  gtk_image_set_from_file(GTK_IMAGE(para->img), "$-green2img");
+  para->green2 = 1;
+  para->green = 0;
+  para->black = 0;
+  para->grey = 0;
+  para->bin = 0;
+  remove("$-green2img");
+  free(img);
+}
+
+void binar (GtkWidget *button , struct param *para)
+{
+  if (para->path == NULL)
+  {
+    return;
+  }
+  SDL_Surface *img;
+  para->but = button;
+  img = load_image(para->path);
+  binarize(img);
+  img = SDL_redim(1100,600,img);
+  SDL_SaveBMP(img, "$-binimg");
+  gtk_image_set_from_file(GTK_IMAGE(para->img), "$-binimg");
+  para->green2 = 0;
+  para->green = 0;
+  para->black = 0;
+  para->grey = 0;
+  para->bin = 1;
+  remove("$-binimg");
+  free(img);
+}
+
 
 void ocr(GtkWidget *button, struct param *para)
 {
@@ -191,7 +281,7 @@ int main(int argc, char* argv[])
   init_sdl();
   GtkWidget *image;
   GtkWidget *b_open, *b_save, *b_grey ,*b_black, *b_reset ,*b_ocr;
-  GtkWidget *b_quit;
+  GtkWidget *b_bin , *b_green ,*b_green2,  *b_quit;
   GtkWidget *pVbox, *pHbox;
   struct param *param = malloc(sizeof(struct param*));
 
@@ -216,18 +306,26 @@ int main(int argc, char* argv[])
   b_black = gtk_button_new_with_mnemonic("Black and white");
   b_reset = gtk_button_new_with_mnemonic("Reset");
   b_ocr = gtk_button_new_with_mnemonic("Start OCR");
+  b_bin = gtk_button_new_with_mnemonic("Binarize");
+  b_green = gtk_button_new_with_mnemonic("Green");
+  b_green2 = gtk_button_new_with_mnemonic("Green2");
   b_quit = gtk_button_new_with_mnemonic("Quit");
   
   param->img = image;
   param->path = NULL;
   param->grey = 0;
   param->black = 0;
+  param->green = 0;
+  param->green2 = 0;
 
   gtk_box_pack_start(GTK_BOX(pHbox), b_open, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_save, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_grey, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_black, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_reset, 1, 0, 0);
+  gtk_box_pack_start(GTK_BOX(pHbox), b_bin, 1, 0, 0);
+  gtk_box_pack_start(GTK_BOX(pHbox), b_green, 1, 0, 0);
+  gtk_box_pack_start(GTK_BOX(pHbox), b_green2, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_ocr, 1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pHbox), b_quit,1, 0, 0);
   gtk_box_pack_start(GTK_BOX(pVbox), image , 1, 0, 0);
@@ -237,6 +335,9 @@ int main(int argc, char* argv[])
   g_signal_connect(G_OBJECT(b_black),"clicked",G_CALLBACK(black),param);
   g_signal_connect(G_OBJECT(b_reset),"clicked",G_CALLBACK(reset_img),param);
   g_signal_connect(G_OBJECT(b_save),"clicked",G_CALLBACK(save_img),param);
+  g_signal_connect(G_OBJECT(b_green),"clicked",G_CALLBACK(to_green),param);
+  g_signal_connect(G_OBJECT(b_green2),"clicked",G_CALLBACK(to_green2),param);
+  g_signal_connect(G_OBJECT(b_bin),"clicked",G_CALLBACK(binar),param);
   g_signal_connect(G_OBJECT(b_ocr),"clicked",G_CALLBACK(ocr),param);
   g_signal_connect(G_OBJECT(b_quit),"clicked",G_CALLBACK(OnDestroy),NULL);
   gtk_widget_show_all(pWindow);
--- Interface/pixel_operations.h
+++ Interface/pixel_operations.h
@@ -1,25 +1,24 @@
-# ifndef IMAGE_OPERATION_H_
-# define IMAGE_OPERATION_H_
-
+// pixel_operations.h
+ 
+# ifndef PIXEL_OPERATIONS_H_
+# define PIXEL_OPERATIONS_H_
+ 
 # include <stdlib.h>
 # include <SDL.h>
-
+ 
 Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
 void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
-void wait_for_keypressed(void);
-SDL_Surface* load_image(char *path) ;
-SDL_Surface* display_image(SDL_Surface *img);
 void init_sdl(void);
+SDL_Surface* load_image(char *path);
 SDL_Surface* grey_lvl(SDL_Surface *img);
-SDL_Surface* black_and_white(SDL_Surface* img);
-SDL_Surface* superposition(SDL_Surface* img);
+SDL_Surface* black_and_white(SDL_Surface *img);
+SDL_Surface* superposition(SDL_Surface *img);
 SDL_Surface* SDL_redim(int W2, int H2, SDL_Surface *img);
-void character_block(SDL_Surface *img);
-SDL_Surface* green(SDL_Surface *img);
-SDL_Surface* green2(SDL_Surface *img);
 void Bloc_Recursif(SDL_Surface *img);
 int* Find_min_and_max_pixels(SDL_Surface *img, int i, int j, int *L);
 SDL_Surface* decoupe_image(SDL_Surface *img, int *L);
+SDL_Surface* green(SDL_Surface *img);
+SDL_Surface* green2(SDL_Surface *img);
 struct tuple* Start(SDL_Surface *img, Uint32 color);
 void Bloc_Green(SDL_Surface *img);
 struct tuple* Start_Green(SDL_Surface *img);
@@ -27,7 +26,8 @@ void Bloc_Iteratif(SDL_Surface *img);
 SDL_Surface* Rogne(SDL_Surface *res);
 void binarize(SDL_Surface *img);
 char *int2char(int x);
-
-
-
+/*int find_next_pixel_notgreen(SDL_Surface *img, int *startx, int *starty);
+int* Find_min_and_max_pixels_ingreenimg(SDL_Surface *img, int x, int y, int *L);
+void Bloc_de_caractere_ingreenimg(SDL_Surface *img, int *startx, int *starty);*/
+ 
 # endif
